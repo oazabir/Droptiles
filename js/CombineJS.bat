ce9d8999b2f6939fae029fd9a629f02a8b7d1acb
Omar AL Zabir <omaralzabir@gmail.com>
2013-08-17 22:28:44 +0100

    CombineJS updated with latest JS

--- js/CombineJS.bat
+++ js/CombineJS.bat
@@ -6,12 +6,13 @@ type jQueryEnhancement.js >> Combined.js
 type jQuery.MouseWheel.js >> Combined.js
 type jquery.kinetic.js >> Combined.js
 type Knockout-2.1.0.js >> Combined.js
+type knockout.sortable.js >> Combined.js
 type cookie.js >> Combined.js
 type bootstrap.min.js >> Combined.js
-type Bootstrap-tooltip.js >> Combined.js
-type Bootstrap.dropdown.js >> Combined.js
 type Underscore.js >> Combined.js
 type jQuery.hashchange.js >> Combined.js
+type jquery.ui.touch-punch.min.js >> Combined.js
+
 type User.js >> Combined.js
 
 del CombinedDashboard.js
