cc9e8873a06f8ff20bb4e13d973a4c372da0978f
Omar AL Zabir <omaralzabir@gmail.com>
2013-08-17 22:59:25 +0100

    Finally fixed combine js problem

--- js/CombineJS.bat
+++ js/CombineJS.bat
@@ -1,7 +1,7 @@
 ﻿
 del Combined.js
 type Underscore.js >> Combined.js
-type jquery-1.9.1.min.js > Combined.js
+type jquery-1.9.1.min.js >> Combined.js
 type jquery-ui-1.10.2.custom.min.js >> Combined.js
 type jQueryEnhancement.js >> Combined.js
 type jQuery.MouseWheel.js >> Combined.js
@@ -16,7 +16,7 @@ type bootstrap.min.js >> Combined.js
 type User.js >> Combined.js
 
 del CombinedDashboard.js
-type TheCore.js > CombinedDashboard.js
+type TheCore.js >> CombinedDashboard.js
 type ..\Tiles\Tiles.js >> CombinedDashboard.js
 type Dashboard.js >> CombinedDashboard.js
 
